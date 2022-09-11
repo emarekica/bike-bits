c1a3242a8ef9945eb3450fc33019a2dfaaf88ab4
emarekica <emarekica@gmail.com>
2022-09-11 22:14:33 +0200

    config(homepage): prepare for contact modal

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,1 +1,19 @@
 // module with innerHTML that opens when "about" clicked
+
+"use strict";
+
+const modal = document.getElementById("contact");
+
+// selects wherever there is id="contact" in HTML
+const openModal = document.querySelector(".contact");
+const closeModal = document.querySelector(".exit");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
+
+// see for importing css through JS: https://stackoverflow.com/questions/574944/how-to-load-up-css-files-using-javascript
